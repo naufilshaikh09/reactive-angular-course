c9805e4ba5d0c945007da875ff559085a3a0793f
Naufil Shaikh <snaufil09@gmail.com>
2023-06-29 19:17:13 +0530

    Added single data observable pattern

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
 import {
@@ -15,16 +15,22 @@ import {
 } from 'rxjs/operators';
 import { merge, fromEvent, Observable, concat } from 'rxjs';
 import { Lesson } from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
   selector: 'course',
   templateUrl: './search-lessons.component.html',
-  styleUrls: ['./search-lessons.component.css']
+  styleUrls: ['./search-lessons.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchLessonsComponent implements OnInit {
 
-  constructor() {
+  searchResults$: Observable<Lesson[]>;
+
+  activeLesson: Lesson;
+
+  constructor(private coursesService: CoursesService) {
 
 
   }
@@ -32,6 +38,18 @@ export class SearchLessonsComponent implements OnInit {
   ngOnInit() {
 
 
+  }
+
+  onSearch(search: string) {
+    this.searchResults$ = this.coursesService.searchLession(search);
+  }
+
+  openLesson(lesson: Lesson) {
+    this.activeLesson = lesson;
+  }
+
+  onBackToSearch() {
+    this.activeLesson = null;
   }
 
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { map, shareReplay } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,26 @@ import { map, shareReplay } from 'rxjs/operators';
 export class CoursesService {
 
   constructor(private http: HttpClient) { }
+
+  loadCourseById(courseId: number) {
+    return this.http.get<Course>(`/api/courses/${courseId}`)
+      .pipe(
+        shareReplay()
+      );
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: "10000",
+        courseId: "" + courseId
+      }
+    })
+      .pipe(
+        map(res => res["payload"]),
+        shareReplay()
+      );
+  }
 
   loadAllCourses(): Observable<Course[]> {
     return this.http.get<Course[]>("/api/courses")
@@ -24,5 +45,18 @@ export class CoursesService {
       .pipe(
         shareReplay()
       );
+  }
+
+  searchLession(search: string): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        filter: search,
+        pageSize: "100"
+      }
+    })
+      .pipe(
+        map(res => res["payload"]),
+        shareReplay()
+      )
   }
 }
